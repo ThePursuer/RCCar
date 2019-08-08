0db7a07871fa5311842a83bfdacbaf7abcea4adf
thepursuer <peterflw@aol.com>
2019-08-08 07:59:36 +0100

    Added L298N support to main's RC car. Set max speed for the RC car.

--- main.cpp
+++ main.cpp
@@ -28,11 +28,10 @@ bool run = true;
 //PCA9685 related
 #define PIN_BASE 300
 #define SERVO_PIN PIN_BASE
-#define MOTOR_PIN (PIN_BASE + 1)
 #define MAX_PWM 4096
 #define HERTZ 330
 
-#define SERVO_MAX_PW 2.5
+#define SERVO_MAX_PW 1.7
 #define SERVO_MIN_PW 0.5
 
 int pca9685FD;
@@ -40,6 +39,11 @@ int pca9685FD;
 //Joystick related
 #define JOYSTICK_FILENAME "/dev/input/js0"
 int joystickfd = -1;
+
+//Motor related
+#define L298N_HBRIDGE1_PIN 38 //GPIO20
+#define L298N_HBRIDGE2_PIN 40 //GPIO21
+#define L298N_EN_PIN (PIN_BASE + 1)
 
 /*
  * Utility function to calculate the ticks needed to produce a pulse of impulseMS milliseconds on the pca9685
@@ -58,12 +62,23 @@ public:
 	MyRCCar(){
 		setServoMaxPw(SERVO_MAX_PW);
 		setServoMinPw(SERVO_MIN_PW);
+
+		setMaxSpeed(MAX_PWM);
 	}
 
 protected:
 	void update(){
-		std::cout << servoPw_ << std::endl;
 		pwmWrite(SERVO_PIN, calcTicks(servoPw_));
+
+		if(goingForward_) {
+			digitalWrite(L298N_HBRIDGE1_PIN, HIGH);
+			digitalWrite(L298N_HBRIDGE2_PIN, LOW);
+		}
+		else {
+			digitalWrite(L298N_HBRIDGE1_PIN, LOW);
+			digitalWrite(L298N_HBRIDGE2_PIN, HIGH);
+		}
+		pwmWrite(L298N_EN_PIN, speed_);
 	}
 };
 
@@ -74,7 +89,10 @@ int main(int argc, char **argv) {
 	 ***************************/
 
 	// Calling wiringPi setup first.
-	wiringPiSetup();
+	wiringPiSetupPhys();
+
+	pinMode(L298N_HBRIDGE1_PIN, OUTPUT);
+	pinMode(L298N_HBRIDGE2_PIN, OUTPUT);
 
 	// Setup with pinbase 300 and i2c location 0x40
 	pca9685FD = pca9685Setup(PIN_BASE, 0x40, HERTZ);
@@ -102,7 +120,7 @@ int main(int argc, char **argv) {
 	 * Set up the RC_Car and Controller *
 	 ************************************/
 
-	std::shared_ptr<MyRCCar> myCar = make_shared<MyRCCar>();
+	shared_ptr<MyRCCar> myCar = make_shared<MyRCCar>();
 	myCar->setUpdateCycle(50);
 	myCar->engineOn();
 
